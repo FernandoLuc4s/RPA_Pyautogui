d2722120df27f2357363a0e80ee6434e48c1aa3c
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-02-19 00:18:42 -0300

    Alterando o link e também as mensagens dos alertas

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time #biblioteca que permite que você controle o tempo de espera
 
-pyautogui.alert("O código vai começar, não mova e não mexa no mouse e no teclado do seu pc")
+pyautogui.alert("A partir deste momento o Bot vai controlar o seu PC, clique no OK e não mova o mouse e não aperte em nenhuma tecla do mouse e teclado")
 # a listas de comandos de cada teclas pode ser encontrada aqui https://pyautogui.readthedocs.io/en/latest/keyboard.html#keyboard-keys
 pyautogui.PAUSE =0.5 #espera meio segundo para executar cada linha de código
 #abrir o google drive
@@ -16,8 +16,6 @@ pyautogui.press('enter')
 time.sleep(4)
 pyautogui.hotkey('winleft', 'd') # o hotkey serve para você fazer combinações de teclas com o python
 
-
-
 # cliquei no arquivo que eu quero fazer backup e arrastar ele
 pyautogui.moveTo(117, 201)# move o mouse para (x,y)
 time.sleep(2)
@@ -31,4 +29,4 @@ pyautogui.hotkey('alt', 'tab')  #muda de uma tela para outra, no caso a ultima a
 pyautogui.mouseUp() # larga o mouse
 # esperar 5 segundos
 time.sleep(5)
-pyautogui.alert("O código acabou de rodar, pode usar o seu computador novamente")
+pyautogui.alert("O trabalho do Bot acabou por aqui, aperte no OK e volte a usar o seu PC ( ͡❛ ͜ʖ ͡❛)")
